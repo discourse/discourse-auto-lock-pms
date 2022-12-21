f97c673b17d6b94d14ea849e02f5a10665e50d72
Penar Musaraj <pmusaraj@gmail.com>
2022-12-21 14:43:46 -0500

    Fix specs

--- spec/integration/topic_spec.rb
+++ spec/integration/topic_spec.rb
@@ -7,7 +7,9 @@ describe Topic do
     SiteSetting.discourse_announcement_pm_enabled = true
   end
 
+  let(:admin) { Fabricate(:admin) }
   let(:user) { Fabricate(:user) }
+  let(:basic_topic_params) { { title: "hello world topic", raw: "my name is fred", archetype: Archetype.private_message } }
 
   context 'when a private message is created' do
     it 'should have the is_announcement_pm custom field' do
@@ -16,9 +18,12 @@ describe Topic do
     end
 
     it 'should set the topic to closed if is_announcement_pm custom field is true' do
-      pm = Fabricate(:private_message_topic, user: user)
-      TopicCustomField.create!(topic_id: pm.id, name: "is_announcement_pm", value: true)
-      put "/t/#{pm.id}.json", params: { is_announcement_pm: true }
+      output = PostCreator.create(admin, basic_topic_params.merge(
+        is_announcement_pm: true,
+        target_usernames: [user.username]
+      ))
+
+      pm = Topic.last
       expect(pm.custom_fields['is_announcement_pm']).to eq(true)
       expect(pm.closed).to eq(true)
     end
