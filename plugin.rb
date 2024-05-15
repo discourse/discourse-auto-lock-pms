63c9e2fb90f0fd04dc240c112c9bf9527e0ad4d4
Loïc Guitaut <loic@discourse.org>
2024-05-15 16:39:08 +0200

    DEV: Update rubocop-discourse to latest version

--- plugin.rb
+++ plugin.rb
@@ -12,28 +12,36 @@ register_asset "stylesheets/common/common.scss"
 enabled_site_setting :discourse_auto_lock_pms_enabled
 
 after_initialize do
-  AUTO_LOCK_FIELD = { name: "auto_lock_pm", type: "boolean" }
+  module ::DiscourseAutoLockPms
+    AUTO_LOCK_FIELD = { name: "auto_lock_pm", type: "boolean" }
+  end
 
-  register_topic_custom_field_type(AUTO_LOCK_FIELD[:name], AUTO_LOCK_FIELD[:type].to_sym)
+  register_topic_custom_field_type(
+    DiscourseAutoLockPms::AUTO_LOCK_FIELD[:name],
+    DiscourseAutoLockPms::AUTO_LOCK_FIELD[:type].to_sym,
+  )
 
   # Getter Method
-  add_to_class(:topic, AUTO_LOCK_FIELD[:name].to_sym) do
-    if !custom_fields[AUTO_LOCK_FIELD[:name]].nil?
-      custom_fields[AUTO_LOCK_FIELD[:name]]
+  add_to_class(:topic, DiscourseAutoLockPms::AUTO_LOCK_FIELD[:name].to_sym) do
+    if !custom_fields[DiscourseAutoLockPms::AUTO_LOCK_FIELD[:name]].nil?
+      custom_fields[DiscourseAutoLockPms::AUTO_LOCK_FIELD[:name]]
     else
       nil
     end
   end
 
   # Setter Method
-  add_to_class(:topic, "#{AUTO_LOCK_FIELD[:name]}=") do |value|
-    custom_fields[AUTO_LOCK_FIELD[:name]] = value
+  add_to_class(:topic, "#{DiscourseAutoLockPms::AUTO_LOCK_FIELD[:name]}=") do |value|
+    custom_fields[DiscourseAutoLockPms::AUTO_LOCK_FIELD[:name]] = value
   end
 
   # Update on Topic Creation
   on(:topic_created) do |topic, opts, user|
     if opts[:archetype] == "private_message"
-      topic.send("#{AUTO_LOCK_FIELD[:name]}=".to_sym, opts[AUTO_LOCK_FIELD[:name].to_sym])
+      topic.send(
+        "#{DiscourseAutoLockPms::AUTO_LOCK_FIELD[:name]}=".to_sym,
+        opts[DiscourseAutoLockPms::AUTO_LOCK_FIELD[:name].to_sym],
+      )
       topic.save!
 
       # Close the topic if auto_lock_pm is enabled
@@ -45,8 +53,15 @@ after_initialize do
   end
 
   # Update on Topic Edit
-  PostRevisor.track_topic_field(AUTO_LOCK_FIELD[:name].to_sym) do |tc, value|
-    tc.record_change(AUTO_LOCK_FIELD[:name], tc.topic.send(AUTO_LOCK_FIELD[:name]), value)
-    tc.topic.send("#{AUTO_LOCK_FIELD[:name]}=".to_sym, value.present? ? value : nil)
+  PostRevisor.track_topic_field(DiscourseAutoLockPms::AUTO_LOCK_FIELD[:name].to_sym) do |tc, value|
+    tc.record_change(
+      DiscourseAutoLockPms::AUTO_LOCK_FIELD[:name],
+      tc.topic.send(DiscourseAutoLockPms::AUTO_LOCK_FIELD[:name]),
+      value,
+    )
+    tc.topic.send(
+      "#{DiscourseAutoLockPms::AUTO_LOCK_FIELD[:name]}=".to_sym,
+      value.present? ? value : nil,
+    )
   end
 end
